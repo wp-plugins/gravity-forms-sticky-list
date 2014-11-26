0ae8431a9a43d9a5947512d6690f78df66f61b33
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 13:19:08 +0000

    Fixed a bug where translations would not get loaded
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-list/trunk@1033164 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky-list.php
+++ sticky-list.php
@@ -31,7 +31,7 @@ if (class_exists("GFForms")) {
             parent::init();
 
             
-            add_action( 'init', array($this, 'stickylist_localize') );   
+            $this->stickylist_localize();
             
             
             add_action("gform_field_standard_settings", array( $this, "stickylist_field_settings"), 10, 2);
