2fafafb8773ead0af7437ee60eba5e18e5f38c58
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 11:27:12 +0000

    Fixed a bug that would show the Sticky List menu item twice
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-list/trunk@1094194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-sticky-list.php
+++ class-sticky-list.php
@@ -4,7 +4,7 @@ if (class_exists("GFForms")) {
 
     class StickyList extends GFAddOn {
 
-        protected $_version = "1.1.9";
+        protected $_version = "1.1.9.1";
         protected $_min_gravityforms_version = "1.8.19.2";
         protected $_slug = "sticky-list";
         protected $_path = "gravity-forms-sticky-list/sticky-list.php";
@@ -1514,10 +1514,6 @@ if (class_exists("GFForms")) {
                 
                 return $original_confirmation;
             }
-
         }
     }
-
-    
-    new StickyList();
 }
