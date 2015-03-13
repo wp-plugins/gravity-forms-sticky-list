c23b22fe68f812fc8ed13d014cf0a68e3428108d
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-13 11:09:13 +0000

    * Feature: Uppdated the shortcode to accept a "showto" parameter
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-list/tags/1.2.7@1111850 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sticky-list.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Gravity Forms Sticky List
+Plugin URI: https://github.com/13pixlar/sticky-list
+Description: List and edit submitted entries from the front end
+Version: 1.2.7
+Author: 13pixar
+Author URI: http://13pixlar.se
+*/
+
+
+/* Todo
+ * Support for file multiple uploads
+ * Support for GF 1.9 "Save and Continue" functionallity
+ * Support for multi page forms
+ */
+
+//------------------------------------------
+add_action( 'gform_loaded', array( 'StickyList_Bootstrap', 'load' ), 5 );
+class StickyList_Bootstrap {
+
+    public static function load() {
+        if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
+            return;
+        }
+
+        require_once( 'class-sticky-list.php' );
+        GFAddOn::register( 'StickyList' );
+    }
+}
