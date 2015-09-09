13faafeb93b9d05aedaf710f7904703ce12e2c56
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-09 12:22:19 +0000

    Updated readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-list/tags/1.4@1241497 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sticky-list.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Gravity Forms Sticky List
+Plugin URI: https://github.com/13pixlar/sticky-list
+Description: List and edit submitted entries from the front end
+Version: 1.4
+Author: 13pixar
+Author URI: http://13pixlar.se
+*/
+
+
+/* Todo
+ * Support for file multiple uploads
+ */
+
+//------------------------------------------
+add_action( 'gform_loaded', array( 'StickyList_Bootstrap', 'load' ), 5 );
+class StickyList_Bootstrap {
+
+    public static function load() {
+        if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
+            return;
+        }
+
+        require_once( 'class-sticky-list.php' );
+        GFAddOn::register( 'StickyList' );
+    }
+}
